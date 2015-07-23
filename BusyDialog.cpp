e3ede1dd23a4a9d512da496acc9215144f9410d0
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2015-07-23 10:06:13 +0200

    enabled c++14 support Updated BusyDialog

--- BusyDialog.cpp
+++ BusyDialog.cpp
@@ -1,17 +1,40 @@
 #include "BusyDialog.h"
 #include "ui_BusyDialog.h"
 
-BusyDialog::BusyDialog(QWidget* parent)
+BusyDialog::BusyDialog(QString title, QWidget* parent)
 	: QDialog(parent, Qt::Window | Qt::CustomizeWindowHint | Qt::WindowTitleHint| Qt::WindowSystemMenuHint)
 	, ui_(new Ui::BusyDialog())
 {
 	ui_->setupUi(this);
+	setWindowTitle(title);
+	show();
 }
 
-void BusyDialog::setText(QString title, QString message)
+BusyDialog::~BusyDialog()
 {
-	setWindowTitle(title);
+	hide();
+}
+
+void BusyDialog::init(QString message, bool percent)
+{
 	ui_->message->setText(message);
+
+	if (percent)
+	{
+		ui_->bar->setMinimum(0);
+		ui_->bar->setMaximum(100);
+		ui_->bar->setValue(0);
+	}
+	else
+	{
+		ui_->bar->setMinimum(0);
+		ui_->bar->setMaximum(0);
+	}
+}
+
+void BusyDialog::update(int value)
+{
+	ui_->bar->setValue(value);
 }
 
 
--- BusyDialog.h
+++ BusyDialog.h
@@ -8,14 +8,23 @@ namespace Ui {
 class BusyDialog;
 }
 
+///Busy/Progress dialog
 class CPPGUISHARED_EXPORT BusyDialog
 	: public QDialog
 {
 	Q_OBJECT
 
 	public:
-		BusyDialog(QWidget *parent = 0);
-		void setText(QString title, QString message);
+		///Constructor
+		BusyDialog(QString title, QWidget* parent = nullptr);
+		///Destructor
+		~BusyDialog();
+
+	public slots:
+		///Sets the message text and mode (busy or progress in percent)
+		void init(QString message, bool percent);
+		///Update percentage progress value.
+		void update(int value);
 
 	private:
 		Ui::BusyDialog* ui_;
