372ae3f5c43e712789f519644302639b2f135475
Marc Sturm <marc.sturm@med.uni-tuebingen.de>
2022-08-05 10:54:41 +0200

    ScrollableTextDialog: added a lot of convenience functions.

--- ScrollableTextDialog.cpp
+++ ScrollableTextDialog.cpp
@@ -1,11 +1,12 @@
 #include "ScrollableTextDialog.h"
 #include "ui_ScrollableTextDialog.h"
 
-ScrollableTextDialog::ScrollableTextDialog(QWidget *parent)
+ScrollableTextDialog::ScrollableTextDialog(QWidget* parent, QString title)
 	: QDialog(parent)
 	, ui_(new Ui::ScrollableTextDialog)
 {
 	ui_->setupUi(this);
+	setWindowTitle(title);
 }
 
 ScrollableTextDialog::~ScrollableTextDialog()
@@ -13,9 +14,45 @@ ScrollableTextDialog::~ScrollableTextDialog()
 	delete ui_;
 }
 
-void ScrollableTextDialog::setText(QString lines)
+void ScrollableTextDialog::setText(const QString& text)
 {
-	ui_->text_browser->setText(lines);
+	ui_->text_browser->setText(text);
+
+	setCursorToStart();
+}
+
+void ScrollableTextDialog::appendLine(const QString& line)
+{
+	ui_->text_browser->append(line);
+
+	setCursorToStart();
+}
+
+void ScrollableTextDialog::appendLines(QStringList& lines)
+{
+	foreach(const QString& line, lines)
+	{
+		appendLine(line);
+	}
+
+	setCursorToStart();
+}
+
+void ScrollableTextDialog::setWordWrapMode(QTextOption::WrapMode mode)
+{
+	ui_->text_browser->setWordWrapMode(mode);
+}
+
+void ScrollableTextDialog::setReadOnly(bool read_only)
+{
+	ui_->text_browser->setReadOnly(read_only);
+}
+
+void ScrollableTextDialog::setCursorToStart()
+{
+	QTextCursor cursor = ui_->text_browser->textCursor();
+	cursor.setPosition(0);
+	ui_->text_browser->setTextCursor(cursor);
 }
 
 
--- ScrollableTextDialog.h
+++ ScrollableTextDialog.h
@@ -3,21 +3,39 @@
 
 #include "cppGUI_global.h"
 #include <QDialog>
+#include <QTextOption>
 
 namespace Ui {
 class ScrollableTextDialog;
 }
 
+//Dialog that shows a scrollable text area
 class CPPGUISHARED_EXPORT ScrollableTextDialog
 	: public QDialog
 {
 	Q_OBJECT
 
 public:
-	ScrollableTextDialog(QWidget* parent = 0);
+	//Constructor
+	ScrollableTextDialog(QWidget* parent = nullptr, QString title = "");
 	~ScrollableTextDialog();
 
-	void setText(QString lines);
+	//Sets text
+	void setText(const QString& text);
+	//Appends a line (adds a newline before it)
+	void appendLine(const QString& line);
+	//Appends lines (adds a newline before earch line)
+	void appendLines(QStringList& lines);
+
+	//Set word wrap mode (default is QTextOption::WrapAtWordBoundaryOrAnywhere)
+	void setWordWrapMode(QTextOption::WrapMode mode);
+
+	//Sets read only property (default is true)
+	void setReadOnly(bool read_only);
+
+protected:
+	//Sets the cursor to the start of the text
+	void setCursorToStart();
 
 private:
 	Ui::ScrollableTextDialog* ui_;
